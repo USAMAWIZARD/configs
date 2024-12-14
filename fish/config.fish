227eba011f88c792f0baeef6b9623844f753894c
USAMAWIZARD <usamawizard@gmail.com>
2024-12-14 10:30:57 +0000

    cofnigs

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+fish_vi_key_bindings
+
+
+if not set -q DISPLAY; and test "$XDG_VTNR" = 1
+    exec startx 
+end
+
+
+set -x N_PREFIX "$HOME/n"; contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+
+set -x N_PREFIX "$HOME/n"; contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+
+set -x N_PREFIX "$HOME/n"; contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
