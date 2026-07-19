b7fee63d190d32c2813c24c20634d7688c9d05f9
USAMAWIZARD <usamawizard@gmail.com>
2026-07-19 22:10:12 +0530

    update config

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,9 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 require("auto-save").setup {}
 
-vim.lsp.enable('clangd')
-vim.lsp.enable('lua_ls')
-
 vim.g.instant_username = "usama"
 require("live-share").setup({
   port_internal = 8888,
@@ -13,23 +10,7 @@ require("live-share").setup({
 })
 local lspconfig = vim.lsp.config
 
-lspconfig('', {
-  on_attach = on_attach,
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  -- root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
-    gopls = {
-      completeUnimported = true,
-      usePlaceholders = true,
-      analyses = {
-        unusedparams = true,
-      },
-    },
-  },
-})
-
-local servers = { "html", "cssls", "lua_ls", "clangd", "gopls","python" }
+local servers = { "html", "cssls", "lua_ls", "clangd", "gopls", "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 local function on_attach(client, bufnr)
@@ -67,70 +48,81 @@ local function on_attach(client, bufnr)
 end
 
 for _, lsp in ipairs(servers) do
-  lspconfig(lsp, {
+  local opts = {
     on_attach = on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-  })
+  }
+
+  if lsp == "gopls" then
+    opts.settings = {
+      gopls = {
+        completeUnimported = true,
+        usePlaceholders = true,
+        analyses = {
+          unusedparams = true,
+        },
+      },
+    }
+  end
+
+  lspconfig(lsp, opts)
+  vim.lsp.enable(lsp)
 end
 
 
-local workspace_dir = "/home/usama/learn/AntMedia/workspace/"
-local jdtlsConfig = {
+local function get_jdtls_config()
+  local root_dir = require("jdtls.setup").find_root { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+  local project_name = vim.fn.fnamemodify(root_dir or vim.fn.getcwd(), ":p:h:t")
+  -- per-project data dir under nvim cache (no need to hardcode paths)
+  local workspace_dir = vim.fn.stdpath "cache" .. "/jdtls-workspace/" .. project_name
 
-  cmd = {
+  local bundles = {
+    vim.fn.glob(
+      "/home/usama/.m2/repository/com/microsoft/java/com.microsoft.java.debug.plugin/0.53.1/com.microsoft.java.debug.plugin-*.jar",
+      true
+    ),
+  }
+  vim.list_extend(bundles, vim.split(vim.fn.glob("/home/usama/tem/vscode-java-test/server/*.jar", true), "\n"))
 
-    "/usr/lib/jvm/java-21-openjdk/bin/java",
-
-    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-    "-Dosgi.bundles.defaultStartLevel=4",
-    "-Declipse.product=org.eclipse.jdt.ls.core.product",
-    "-Dlog.protocol=true",
-    "-Dlog.level=ALL",
-    "-Xmx1g",
-    "--add-modules=ALL-SYSTEM",
-    "--add-opens",
-    "java.base/java.util=ALL-UNNAMED",
-    "--add-opens",
-    "java.base/java.lang=ALL-UNNAMED",
-    "-jar",
-    "/home/usama/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.7.100.v20251111-0406.jar",
-    "-configuration",
-    "/home/usama/.local/share/nvim/mason/packages/jdtls/config_linux/",
-    -- 💀
-    -- See `data directory configuration` section in the README
-    "-data",
-    workspace_dir,
-  },
-  settings = {
-    -- settings configuration
-  },
-  init_options = {
-    bundles = {
+  return {
+    cmd = {
+      "/usr/lib/jvm/java-21-openjdk/bin/java",
+      "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+      "-Dosgi.bundles.defaultStartLevel=4",
+      "-Declipse.product=org.eclipse.jdt.ls.core.product",
+      "-Dlog.protocol=true",
+      "-Dlog.level=ALL",
+      "-Xmx1g",
+      "--add-modules=ALL-SYSTEM",
+      "--add-opens",
+      "java.base/java.util=ALL-UNNAMED",
+      "--add-opens",
+      "java.base/java.lang=ALL-UNNAMED",
+      "-jar",
+      "/home/usama/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.7.100.v20251111-0406.jar",
+      "-configuration",
+      "/home/usama/.local/share/nvim/mason/packages/jdtls/config_linux/",
+      "-data",
+      workspace_dir,
     },
-  },
-}
-local bundles = {
-  vim.fn.glob(
-    "/home/usama/.m2/repository/com/microsoft/java/com.microsoft.java.debug.plugin/0.53.1/com.microsoft.java.debug.plugin-*.jar",
-    true),
-};
-
-vim.list_extend(bundles, vim.split(vim.fn.glob("/home/usama/tem/vscode-java-test/server/*.jar", true), "\n"))
-jdtlsConfig.init_options = {
-  bundles = bundles,
-}
-
-jdtlsConfig.on_attach = function(client, bufnr)
-  on_attach(client, bufnr)
-  require('jdtls').setup_dap({ hotcodereplace = 'auto', config_overrides = {} })
+    root_dir = root_dir,
+    settings = {},
+    init_options = {
+      bundles = bundles,
+    },
+    on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
+      require("jdtls").setup_dap { hotcodereplace = "auto", config_overrides = {} }
+    end,
+  }
 end
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "java",
   callback = function()
-    require('jdtls').start_or_attach(jdtlsConfig)
-  end
+    require("jdtls").start_or_attach(get_jdtls_config())
+  end,
 })
 
 vim.opt.spelllang = 'en_us'
